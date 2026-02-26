cab1d5c46c7285803bd3258dbab5df9930be0146
Lestat Alvarez Quintana Ordiz <lestatquintana@gmail.com>
2026-02-26 17:55:13 +0000

    Added the "tools" that will allow us make different firing types and modify soread. (Burst, Single, Automatic)

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,17 +1,19 @@
 using System;
 using System.Collections;
 using Unity.Mathematics;
+using UnityEditor;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.Scripting;
 
 public class Weapon : MonoBehaviour
 {
-    //public Camera playerCamera;
+    public Camera playerCamera;
     public GameObject bulletPrefab; //The bullet
     public Transform bulletSpawn; //The bullet spawner
     public float bulletVelocity = 25; //The default velocity of the bullet
     public float bulletLifetime = 3f; //The bullet "air time"
-    public enum WeaponType
+    public enum WeaponType //Shooting types
     {
         Single,
         Burst,
@@ -27,7 +29,7 @@ public class Weapon : MonoBehaviour
     public int bulletsPerBurst = 3; //If shooting a burst of bullets it would be how many bullets per burst-
     public int currentBurst; //To work with the burst that just got shot. and not letting it behave like a full automatic.
 
-    public WeaponType currentType;
+    public WeaponType currentType; //how we are going to compare in if statements for the weapon to have the corresponding behaviours.
 
     private void Awake()
     {
@@ -37,20 +39,85 @@ public class Weapon : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (currentType == WeaponType.Automatic)
         {
+            //Only shoots if holding the click
+            isShooting = Input.GetKey(KeyCode.Mouse0);//GetKey is for holding the button
+        }
+        else if (currentType == WeaponType.Single || currentType == WeaponType.Burst)
+        {
+            isShooting = Input.GetKeyDown(KeyCode.Mouse0); //GetKeyDown when pressing only once.
+        }
+        if (readyShooting && isShooting)
+        {
+            currentBurst = bulletsPerBurst;
             FireWeapon();
         }
     }
 
+    public void ResetShooting() //making sure that we are allowed to shoot, and allowing to reset the shooting in case we start the process of shooting.
+    {
+        readyShooting = true;
+        allowReset = true;
+    }
+
     private void FireWeapon()
     {
+        //We said that we cant start shooting once the shooting started.    
+        readyShooting = false;
+        Vector3 directionOfShot = DirectionAndSpreadCal().normalized;
+
         //Instantiating the bullet.
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+
+        //This is a pointer, points at the direction we are shooting.
+        bullet.transform.forward = directionOfShot;
+        //Force that shoots the bullet from spawn position (gun) in certain direction. (Foward is the blue axis on the little compass thingy).
+
         //Shooting the bullet
-        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse); //Force that shoots the bullet from spawn position (gun) in certain direction. (Foward is the blue axis on the little compass thingy). "Impulse" is the way that the force will work.
+        bullet.GetComponent<Rigidbody>().AddForce(directionOfShot * bulletVelocity, ForceMode.Impulse);//"Impulse" is the way that the force will work.
 
         StartCoroutine(DestroyBullet(bullet, bulletLifetime)); //Removes the bullet after certain delayed applied to the bullet.
+
+        //The same way we check if we are allowed to start shooting we check if we are done shooting.
+        if (allowReset)
+        {
+            Invoke("ResetShooting", delayShot);
+            allowReset = false;
+        }
+
+        //Checking if we are shooting in burst
+        if (currentType == WeaponType.Burst && currentBurst > 1) //Makes sure that if the weapon has still bullets to be shot in this burst reduces the amount left.
+        {
+            currentBurst--;
+            Invoke("FireWeapon", delayShot);
+        }
+    }
+
+
+    public Vector3 DirectionAndSpreadCal()
+    {
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))//When we shoot at something we get the direction of where the bullet has to go.
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {//When we shoot to nothing like the air, is how we get the flying direction of the bullet.
+            targetPoint = ray.GetPoint(100);
+        }
+
+        Vector3 direction = targetPoint - bulletSpawn.position;
+
+        //Creating the random spread of bullets. The spread varies on both axys.
+        float x = UnityEngine.Random.Range(-shootingSpread, shootingSpread);
+        float y = UnityEngine.Random.Range(-shootingSpread, shootingSpread);
+
+        //Returning spread and direction
+        return direction + new Vector3(x, y, 0);
     }
 
     private IEnumerator DestroyBullet(GameObject bullet, float bulletLifetime)
